90921a1995c6297dc8a9eace4f3d7eca6a71920b
Mason Mackaman <masondeanm@aol.com>
2020-02-26 14:35:26 -0500

    Change to point-free

--- src/Console/Random.elm
+++ src/Console/Random.elm
@@ -6,7 +6,7 @@ import Random exposing (Generator)
 
 generate : (a -> msg) -> Generator a -> Cmd log msg
 generate toMsg generator =
-    Random.generate (\a -> UpdateMsg <| toMsg a) generator
+    Random.generate (UpdateMsg << toMsg) generator
 
 
 int : Int -> Int -> Generator Int
